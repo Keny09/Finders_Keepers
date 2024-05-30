8cb13c50d4ee8c0068fd95517098b102ece62c94
Keny09 <rosekendy09@gmail.com>
2024-05-30 14:22:22 +0300

    Images

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import '../NewCollections.css'
+import './NewCollections.css'
 import new_collection from '../Assets/new_collections'
 import Item from '../Item/Item'
 
 const NewCollections = () => {
   return (
-    <div className='newcollections'>
+    <div className='new-collections'>
         <h1>NEW COLLECTIONS</h1>
         <hr/>
         <div className="collections">
