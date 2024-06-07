022eed9c7457d862982e4af613a4b5d60d3c1212
Keny09 <rosekendy09@gmail.com>
2024-06-07 06:31:13 +0300

    Related Products Update

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -7,7 +7,7 @@ const Item = (props) => {
     <div className='item'>
         <Link to={`/product/${props.id}`}><img src={props.image} alt='' /></Link>
         <p>{props.name}</p>
-        <div className="Item-prices">
+        <div className="item-prices">
             <div className="item-price-new">
               ${props.new_price}
             </div>
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
-import Breadcrumbs from '../Components/Breadcrumbs/Breadcrumbs';
+import Breadcrumb from '../Components/Breadcrumbs/Breadcrumb';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DescriptonBox from '../Components/DescriptonBox/DescriptonBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -10,8 +12,10 @@ const Product = () => {
   const product = all_product.find((e)=> e.id === Number(productId));
   return (
     <div>
-      <Breadcrumbs product={product}/>
+      <Breadcrumb product={product}/>
       <ProductDisplay product={product}/>
+      <DescriptonBox/>
+      <RelatedProducts/>
     </div>
   )
 }
