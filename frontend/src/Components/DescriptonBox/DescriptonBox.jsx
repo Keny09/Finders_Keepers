66c13f700f83cd910724af6e666feb13eff440f7
Rose Kendy <rosekendy09@gmail.com>
2024-07-15 17:15:55 +0200

    frontend

---
+++ frontend/src/Components/DescriptonBox/DescriptonBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptonBox.css'
+
+const DescriptonBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>The Finders Keepers is a haven for treasure seekers, specializing in breathing new life into previously loved finds. We curate a vast selection of pre-owned gems, including timeless vintage attire, unique accessories, charming home décor, and one-of-a-kind collectibles. Whether you're looking for a touch of nostalgia or a distinctive addition to your home, Finders Keepers offers an unparalleled adventure in pre-loved shopping</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptonBox
